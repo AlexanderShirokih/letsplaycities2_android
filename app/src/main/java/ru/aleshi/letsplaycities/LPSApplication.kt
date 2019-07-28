32396837753a8863a4ebb84cf80828998d2bfd4b
Alexander Shirokih <alexandershirokih98@gmail.com>
2019-07-28 15:31:28 +0300

    remove multidex library

--- app/src/main/java/ru/aleshi/letsplaycities/LPSApplication.kt
+++ app/src/main/java/ru/aleshi/letsplaycities/LPSApplication.kt
@@ -1,11 +1,11 @@
 package ru.aleshi.letsplaycities
 
-import androidx.multidex.MultiDexApplication
+import android.app.Application
 import ru.aleshi.letsplaycities.base.GamePreferences
 import ru.aleshi.letsplaycities.social.ServiceType
 import ru.aleshi.letsplaycities.social.SocialNetworkManager
 
-class LPSApplication : MultiDexApplication() {
+class LPSApplication :Application() {
 
     val gamePreferences: GamePreferences by lazy {
         GamePreferences(this)
