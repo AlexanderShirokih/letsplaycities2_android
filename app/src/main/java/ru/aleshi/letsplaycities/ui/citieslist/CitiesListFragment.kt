9233a56c21563f23d67a40c78606f367e412a680
alexander <alexandrshirokih@mail.ru>
2020-03-18 23:14:55 +0300

    add cities list fragment

---
+++ app/src/main/java/ru/aleshi/letsplaycities/ui/citieslist/CitiesListFragment.kt
@@ -0,0 +1,27 @@
+package ru.aleshi.letsplaycities.ui.citieslist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ru.aleshi.letsplaycities.R
+
+class CitiesListFragment : Fragment() {
+
+    private lateinit var viewModel: CitiesListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.cities_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this)[CitiesListViewModel::class.java]
+    }
+
+}
