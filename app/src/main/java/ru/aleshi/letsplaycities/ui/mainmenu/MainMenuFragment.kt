15578b94566a3ff221e10ad7d4a0d6e43010d29c
alexander <alexandrshirokih@mail.ru>
2020-03-18 18:59:30 +0300

    add new menu items; move play buttons to nested screen

--- app/src/main/java/ru/aleshi/letsplaycities/ui/mainmenu/MainMenuFragment.kt
+++ app/src/main/java/ru/aleshi/letsplaycities/ui/mainmenu/MainMenuFragment.kt
@@ -1,12 +1,8 @@
 package ru.aleshi.letsplaycities.ui.mainmenu
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils.makeInAnimation
-import android.view.animation.AnimationUtils.makeOutAnimation
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -21,10 +17,11 @@ import ru.aleshi.letsplaycities.ui.MainActivity
 import ru.aleshi.letsplaycities.ui.game.GameSessionViewModel
 import javax.inject.Inject
 
-class MainMenuFragment : Fragment(), MainMenuContract.MainMenuView {
+class MainMenuFragment : Fragment(R.layout.fragment_main_menu), MainMenuContract.MainMenuView {
 
     @Inject
     lateinit var presenter: MainMenuContract.MainMenuPresenter
+
     @Inject
     lateinit var prefs: GamePreferences
 
@@ -33,25 +30,21 @@ class MainMenuFragment : Fragment(), MainMenuContract.MainMenuView {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main_menu, container, false)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        (requireActivity() as MainActivity).setToolbarVisibility(false)
-        checkRateDialog()
-        makeInAnimation()
-        createViewListener().run {
-            btn_pva.setOnClickListener(this)
-            btn_pvp.setOnClickListener(this)
-            btn_mul.setOnClickListener(this)
-            btn_net.setOnClickListener(this)
-            btn_set.setOnClickListener(this)
+        val activity = (requireActivity() as MainActivity)
+        activity.setToolbarVisibility(false)
+        activity.onBackPressedDispatcher.addCallback(this@MainMenuFragment) {
+            motionLayout.apply {
+                if (currentState != R.id.start) {
+                    val t = getTransition(R.id.endToStart)
+                    setTransition(t.startConstraintSetId, t.endConstraintSetId)
+                    transitionToEnd()
+                } else
+                    activity.finish()
+            }
         }
+        checkRateDialog()
+        setupPlayButtonListeners()
     }
 
     private fun checkRateDialog() {
@@ -60,71 +53,27 @@ class MainMenuFragment : Fragment(), MainMenuContract.MainMenuView {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        makeInAnimation()
-    }
-
-    private fun createViewListener(): View.OnClickListener {
-        return View.OnClickListener { v ->
-            makeOutAnimation(Runnable {
-
-                val navController = findNavController()
-                when (v.id) {
-                    R.id.btn_pva -> presenter.startGame(false)
-                    R.id.btn_pvp -> presenter.startGame(true)
-                    R.id.btn_mul -> navController.navigate(R.id.start_multiplayer_fragment)
-                    R.id.btn_net -> navController.navigate(
-                        MainMenuFragmentDirections.startNetworkFragment(
-                            BuildConfig.HOST
-                        )
+    private fun setupPlayButtonListeners() {
+        val clickListener = View.OnClickListener { v ->
+            val navController = findNavController()
+            when (v.id) {
+                R.id.btn_pva -> presenter.startGame(false)
+                R.id.btn_pvp -> presenter.startGame(true)
+                R.id.btn_mul -> navController.navigate(R.id.start_multiplayer_fragment)
+                R.id.btn_net -> navController.navigate(
+                    MainMenuFragmentDirections.startNetworkFragment(
+                        BuildConfig.HOST
                     )
-                    R.id.btn_set -> navController.navigate(R.id.start_settings_fragment)
-                    else -> throw IllegalStateException("Unknown button clicked!")
-                }
-            })
+                )
+                R.id.btn_set -> navController.navigate(R.id.start_settings_fragment)
+                else -> throw IllegalStateException("Unknown button clicked!")
+            }
         }
-    }
-
-
-    private fun makeInAnimation() {
-        startAnimation(btn_pva, 0, true, null)
-        startAnimation(btn_pvp, 200, true, null)
-        startAnimation(btn_net, 400, true, null)
-        startAnimation(btn_mul, 600, true, null)
-        startAnimation(btn_set, 800, true, null)
-    }
-
-    private fun makeOutAnimation(fb: Runnable) {
-        startAnimation(btn_set, 0, false, null)
-        startAnimation(btn_mul, 200, false, null)
-        startAnimation(btn_net, 400, false, null)
-        startAnimation(btn_pvp, 600, false, null)
-        startAnimation(btn_pva, 800, false, fb)
-    }
-
-    private fun startAnimation(v: View, delay: Int, inAnimation: Boolean, fb: Runnable?) {
-        val animation =
-            if (inAnimation) makeInAnimation(requireContext(), true) else makeOutAnimation(
-                requireContext(),
-                true
-            )
-        animation.duration = 500
-        animation.startOffset = delay.toLong()
-        if (!inAnimation) {
-            animation.setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation) {}
-
-                override fun onAnimationEnd(animation: Animation) {
-                    v.visibility = View.INVISIBLE
-                    fb?.run()
-                }
-
-                override fun onAnimationRepeat(animation: Animation) {}
-            })
-        } else
-            v.visibility = View.VISIBLE
-        v.startAnimation(animation)
+        btn_pva.setOnClickListener(clickListener)
+        btn_pvp.setOnClickListener(clickListener)
+        btn_mul.setOnClickListener(clickListener)
+        btn_net.setOnClickListener(clickListener)
+        btn_set.setOnClickListener(clickListener)
     }
 
     override fun startGame(gameSession: GameSession) {
